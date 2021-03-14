344c80dff1fac2545e102c740337f39cb429aeb1
Hain Balázs <hainb@sch.bme.hu>
2021-03-15 00:27:52 +0100

    finished task 3

--- hu.bme.mit.yakindu.analysis/yakindu-gen/hu/bme/mit/yakindu/analysis/example/ExampleStatemachine.java
+++ hu.bme.mit.yakindu.analysis/yakindu-gen/hu/bme/mit/yakindu/analysis/example/ExampleStatemachine.java
@@ -24,12 +24,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			black = true;
 		}
 		
-		private boolean arbiter;
-		
-		public void raiseArbiter() {
-			arbiter = true;
-		}
-		
 		private long whiteTime;
 		
 		public long getWhiteTime() {
@@ -54,7 +48,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			start = false;
 			white = false;
 			black = false;
-			arbiter = false;
 		}
 	}
 	
@@ -66,7 +59,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 		main_region_Init,
 		main_region_Black,
 		main_region_White,
-		main_region_ArbiterIntervened,
 		$NullState$
 	};
 	
@@ -125,9 +117,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			case main_region_White:
 				main_region_White_react(true);
 				break;
-			case main_region_ArbiterIntervened:
-				main_region_ArbiterIntervened_react(true);
-				break;
 			default:
 				// $NullState$
 			}
@@ -181,8 +170,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			return stateVector[0] == State.main_region_Black;
 		case main_region_White:
 			return stateVector[0] == State.main_region_White;
-		case main_region_ArbiterIntervened:
-			return stateVector[0] == State.main_region_ArbiterIntervened;
 		default:
 			return false;
 		}
@@ -226,10 +213,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 	
 	public void raiseBlack() {
 		sCInterface.raiseBlack();
-	}
-	
-	public void raiseArbiter() {
-		sCInterface.raiseArbiter();
 	}
 	
 	public long getWhiteTime() {
@@ -288,12 +271,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 		stateVector[0] = State.main_region_White;
 	}
 	
-	/* 'default' enter sequence for state ArbiterIntervened */
-	private void enterSequence_main_region_ArbiterIntervened_default() {
-		nextStateIndex = 0;
-		stateVector[0] = State.main_region_ArbiterIntervened;
-	}
-	
 	/* 'default' enter sequence for region main region */
 	private void enterSequence_main_region_default() {
 		react_main_region__entry_Default();
@@ -321,12 +298,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 		exitAction_main_region_White();
 	}
 	
-	/* Default exit sequence for state ArbiterIntervened */
-	private void exitSequence_main_region_ArbiterIntervened() {
-		nextStateIndex = 0;
-		stateVector[0] = State.$NullState$;
-	}
-	
 	/* Default exit sequence for region main region */
 	private void exitSequence_main_region() {
 		switch (stateVector[0]) {
@@ -338,9 +309,6 @@ public class ExampleStatemachine implements IExampleStatemachine {
 			break;
 		case main_region_White:
 			exitSequence_main_region_White();
-			break;
-		case main_region_ArbiterIntervened:
-			exitSequence_main_region_ArbiterIntervened();
 			break;
 		default:
 			break;
@@ -387,12 +355,7 @@ public class ExampleStatemachine implements IExampleStatemachine {
 						
 						enterSequence_main_region_Black_default();
 					} else {
-						if (sCInterface.arbiter) {
-							exitSequence_main_region_Black();
-							enterSequence_main_region_ArbiterIntervened_default();
-						} else {
-							did_transition = false;
-						}
+						did_transition = false;
 					}
 				}
 			}
@@ -415,25 +378,9 @@ public class ExampleStatemachine implements IExampleStatemachine {
 						
 						enterSequence_main_region_White_default();
 					} else {
-						if (sCInterface.arbiter) {
-							exitSequence_main_region_White();
-							enterSequence_main_region_ArbiterIntervened_default();
-						} else {
-							did_transition = false;
-						}
+						did_transition = false;
 					}
 				}
-			}
-		}
-		return did_transition;
-	}
-	
-	private boolean main_region_ArbiterIntervened_react(boolean try_transition) {
-		boolean did_transition = try_transition;
-		
-		if (try_transition) {
-			if (react()==false) {
-				did_transition = false;
 			}
 		}
 		return did_transition;
